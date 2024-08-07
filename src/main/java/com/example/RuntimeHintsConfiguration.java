ddc0530f3bf8d6dc3fa7c384a14d301c8551020a
Ömer Çelik <34758411+omercelikceng@users.noreply.github.com>
2024-08-07 07:55:51 +0300

    Update RuntimeHintsConfiguration.java

--- src/main/java/com/example/RuntimeHintsConfiguration.java
+++ src/main/java/com/example/RuntimeHintsConfiguration.java
@@ -1,4 +1,26 @@
 package com.example;
 
+import com.hazelcast.instance.GeneratedBuildProperties;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportRuntimeHints;
+
+@ImportRuntimeHints(RuntimeHintsConfiguration.BindingRuntimeHints.class)
+@Configuration("cacheRuntimeHintsConfiguration")
 public class RuntimeHintsConfiguration {
+
+    static class BindingRuntimeHints implements RuntimeHintsRegistrar {
+        @Override
+        public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+            try {
+                // With the new hazelcast version we no longer need to add this.
+                // However, since the new version has not been released I had to add this.
+                // Resolved : https://github.com/hazelcast/hazelcast/pull/26386
+                hints.reflection().registerField(GeneratedBuildProperties.class.getDeclaredField("ARTIFACT_ID"));
+            } catch (NoSuchFieldException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
